6dab5578b0a5d667d9f8286da52ab083b2dfe914
Ryan <rselliott99@gmail.com>
2020-10-18 01:40:15 -0400

    Switch to InteractiveClasScreen

--- App.js
+++ App.js
@@ -2,11 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import CourseViewScreen from './app/screens/CourseViewScreen.js';
+import InteractiveClassScreen from './app/screens/InteractiveClassScreen.js';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <CourseViewScreen />
+      <InteractiveClassScreen />
     </View>
   );
 }
